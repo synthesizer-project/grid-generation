17c4e1b0b82ceebdb41dd9918e864de955a2981b
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2025-03-04 08:24:41 +0000

    updated argument

--- src/synthesizer_grids/incident/blackholes/install_relqso.py
+++ src/synthesizer_grids/incident/blackholes/install_relqso.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     parser = Parser(description="RELQSO AGN model creation.")
 
     # parameter file to use
-    parser.add_argument("-config_file", type=str, required=True)
+    parser.add_argument("--config-file", type=str, required=True)
 
     # get the arguments
     args = parser.parse_args()
